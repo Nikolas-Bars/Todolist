5232eb70945b68fb758efe8426f731922b427725
Nikolas-Bars <kasp2409@mail.ru>
2022-06-18 19:37:42 +0300

    Updates

---
+++ precache-manifest.a5890ef91cf3533c16ad6fc669de754d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f787ce3a7d4c6de59c6c0bf802d61832",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "f689e7dabe454c2f2026",
+    "url": "/Todolist/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "0a07ae672923d9a59ae7",
+    "url": "/Todolist/static/js/2.2a36cb1c.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/Todolist/static/js/2.2a36cb1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f689e7dabe454c2f2026",
+    "url": "/Todolist/static/js/main.3cc4d054.chunk.js"
+  },
+  {
+    "revision": "8c9ba947015c2dd63600",
+    "url": "/Todolist/static/js/runtime-main.e8babc40.js"
+  }
+]);
